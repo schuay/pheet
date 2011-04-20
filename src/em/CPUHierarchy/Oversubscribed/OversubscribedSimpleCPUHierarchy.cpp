7b4aba1f0ebca562bcb2e93e488a1f365e90f42f
Martin Wimmer <martin.wimmer@sanethis.com>
2011-04-20 15:19:09 +0200

    started working on oversubscribed simple cpu hierarchy

---
+++ src/em/CPUHierarchy/Oversubscribed/OversubscribedSimpleCPUHierarchy.cpp
@@ -0,0 +1,21 @@
+/*
+ * OversubscribedSimpleCPUHierarchy.cpp
+ *
+ *  Created on: 20.04.2011
+ *      Author: mwimmer
+ */
+
+#include "OversubscribedSimpleCPUHierarchy.h"
+
+namespace pheet {
+
+OversubscribedSimpleCPUHierarchy::OversubscribedSimpleCPUHierarchy() {
+	// TODO Auto-generated constructor stub
+
+}
+
+OversubscribedSimpleCPUHierarchy::~OversubscribedSimpleCPUHierarchy() {
+	// TODO Auto-generated destructor stub
+}
+
+}
---
+++ src/em/CPUHierarchy/Oversubscribed/OversubscribedSimpleCPUHierarchy.h
@@ -0,0 +1,23 @@
+/*
+ * OversubscribedSimpleCPUHierarchy.h
+ *
+ *  Created on: 20.04.2011
+ *      Author: mwimmer
+ */
+
+#ifndef OVERSUBSCRIBEDSIMPLECPUHIERARCHY_H_
+#define OVERSUBSCRIBEDSIMPLECPUHIERARCHY_H_
+
+#include "../Simple/SimpleCPUHierarchy.h"
+
+namespace pheet {
+
+class OversubscribedSimpleCPUHierarchy {
+public:
+	OversubscribedSimpleCPUHierarchy();
+	~OversubscribedSimpleCPUHierarchy();
+};
+
+}
+
+#endif /* OVERSUBSCRIBEDSIMPLECPUHIERARCHY_H_ */
