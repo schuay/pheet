b53230312ecdcd44abbe61dc63d1097826527297
Daniel Cederman <cederman@chalmers.se>
2011-11-02 17:14:55 +0100

    Fixed missing code in userdefprio

--- src/test/inarow/InARowTask.h
+++ src/test/inarow/InARowTask.h
@@ -11,6 +11,7 @@
 
 #include "../../misc/types.h"
 #include "../../misc/atomics.h"
+#include "../test_schedulers.h"
 #include <string.h>
 #include <stdlib.h>
 #include <iostream>
@@ -102,7 +103,7 @@ namespace pheet {
 						continue;
 
 					
-					tec.template spawn<InARowTask<Task> >(depth-1, newboard, i, !player, &vals[i],iar, boardval, debug&& i==0);
+					tec.template spawn_prio<InARowTask<Task> >(UserDefinedPriority(100-depth,depth), depth-1, newboard, i, !player, &vals[i],iar, boardval, debug&& i==0);
 				}
 			}
 
