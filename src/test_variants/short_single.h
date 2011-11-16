809d6dcd3a861de4e517e70653618bd242ecb91c
Martin Wimmer <martin@wimmer.co.uk>
2011-11-16 16:25:32 +0100

    more consistent configuration

--- src/test_variants/short_single.h
+++ src/test_variants/short_single.h
@@ -6,21 +6,21 @@
  *     License: Pheet license
  */
 
-#ifndef LONG_SINGLE_H_
-#define LONG_SINGLE_H_
+#ifndef SHORT_SINGLE_H_
+#define SHORT_SINGLE_H_
 
 #include "../misc/types.h"
 
 namespace pheet {
 
 const bool sorting_test = true;
-const procs_t sorting_test_cpus[] = {1, 2, 4, 8};
+const procs_t sorting_test_cpus[] = {1, 2, 4, 8, 16, 32};
 const unsigned int sorting_test_seeds[] = {0};
 const size_t sorting_test_n[] = {10000000};
 const int sorting_test_types[] = {0};
 
 const bool graph_bipartitioning_test = true;
-const procs_t graph_bipartitioning_test_cpus[] = {1, 2, 4, 8};
+const procs_t graph_bipartitioning_test_cpus[] = {1, 2, 4, 8, 16, 32};
 const unsigned int graph_bipartitioning_test_seeds[] = {0};
 const size_t graph_bipartitioning_test_n[] = {24};
 const double graph_bipartitioning_test_p[] = {0.5};
@@ -29,4 +29,4 @@ const int graph_bipartitioning_test_types[] = {0};
 const bool inarow_test = false;
 }
 
-#endif /* LONG_SINGLE_H_ */
+#endif /* SHORT_SINGLE_H_ */
