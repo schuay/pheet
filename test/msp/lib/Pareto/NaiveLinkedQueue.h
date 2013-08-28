99b713fcf5528c6b2fc748f2275e69ed91cae93e
Jakob Gruber <jakob.gruber@gmail.com>
2013-08-28 12:42:29 +0200

    MSP: Add license headers

--- test/msp/lib/Pareto/NaiveLinkedQueue.h
+++ test/msp/lib/Pareto/NaiveLinkedQueue.h
@@ -1,3 +1,9 @@
+/*
+ * Copyright Jakob Gruber, Martin Kalany 2013.
+ * Distributed under the Boost Software License, Version 1.0.
+ * (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+ */
+
 #ifndef __NAIVE_LINKED_QUEUE_H
 #define __NAIVE_LINKED_QUEUE_H
 
