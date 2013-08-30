ab83615205a7790ad48eb0b9278968cf178b3e40
Martin Kalany <m.kalany@gmx.at>
2013-08-30 11:50:56 +0200

    MSP: Add a class mapping pareto sets to graph nodes

---
+++ test/msp/lib/Pareto/SetMapper.cpp
@@ -0,0 +1,45 @@
+/*
+ * Copyright Jakob Gruber, Martin Kalany 2013.
+ * Distributed under the Boost Software License, Version 1.0.
+ * (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+ */
+
+#include "LockedSet.h"
+#include "SetMapper.h"
+
+namespace pareto
+{
+
+SetMapper::
+SetMapper(graph::Graph const& g)
+{
+	for (auto node : g.nodes()) {
+		LockedSet* set = new LockedSet();
+		map.insert(std::pair<const graph::Node*, LockedSet*>(node, set));
+	}
+}
+
+SetMapper::
+~SetMapper()
+{
+	for (auto it = map.begin(); it != map.end(); ++it) {
+		delete it->second;
+	}
+
+}
+
+sp::Paths
+SetMapper::
+insert(sp::Paths& paths)
+{
+	sp::Paths non_dominated;
+	for (auto p : paths) {
+		const graph::Node* node = p->head();
+		if (map[node]->insert(p)) {
+			non_dominated.push_back(p);
+		}
+	}
+	return non_dominated;
+}
+
+}
