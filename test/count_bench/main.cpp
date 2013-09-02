c3a6592ee55b59eea97df5c4ea0da7592a367943
Jakob Gruber <jakob.gruber@gmail.com>
2013-09-02 17:18:48 +0200

    CMake: Provide a separate main.cpp for each test
    Another option would have been to provide a single main.cpp.in,
    and let CMake configure it during the build.

---
+++ test/count_bench/main.cpp
@@ -0,0 +1,20 @@
+/*
+ * Copyright Martin Wimmer 2013.
+ * Distributed under the Boost Software License, Version 1.0.
+ * (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+ */
+
+#define NDEBUG 1
+
+#include "../init.h"
+#include "CountBench.h"
+
+using namespace pheet;
+
+int main(int, char**)
+{
+	CountBench t;
+	t.run_test();
+
+	return 0;
+}
---
+++ test/tristrip/main.cpp
@@ -0,0 +1,22 @@
+/*
+ * Copyright Martin Wimmer 2013.
+ * Distributed under the Boost Software License, Version 1.0.
+ * (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+ */
+
+#define NDEBUG 1
+
+#include "../init.h"
+#include "TriStripTests.h"
+
+#define USES_STRATEGY (true)
+
+using namespace pheet;
+
+int main(int, char**)
+{
+	TriStripTests t;
+	t.run_test(USES_STRATEGY);
+
+	return 0;
+}
