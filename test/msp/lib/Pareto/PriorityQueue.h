6f0e22e5e139d3cc6f742dcdc9f7baae819433c0
Jakob Gruber <jakob.gruber@gmail.com>
2013-09-04 11:41:51 +0200

    MSP: Implement the Pareto priority queue

--- test/msp/lib/Pareto/PriorityQueue.h
+++ test/msp/lib/Pareto/PriorityQueue.h
@@ -20,15 +20,19 @@ namespace pareto
 class PriorityQueue : public Queue
 {
 public:
-	PriorityQueue();
-	virtual ~PriorityQueue();
-
 	sp::PathPtr first() override;
 	void insert(sp::PathPtr path) override;
 	bool empty() const override;
 
 private:
-	std::priority_queue<sp::PathPtr> m_queue;
+	class path_lincomb_greater
+	{
+	public:
+		bool operator()(sp::PathPtr const& lhs, sp::PathPtr const& rhs) const;
+	};
+
+private:
+	std::priority_queue<sp::PathPtr, std::vector<sp::PathPtr>, path_lincomb_greater> m_queue;
 };
 
 }
