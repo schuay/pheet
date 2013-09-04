b546eba581a99fb7cda4fb2cceadeedb43735611
Jakob Gruber <jakob.gruber@gmail.com>
2013-09-04 11:24:30 +0200

    MSP: Skeleton for a Pareto priority queue implementation

---
+++ test/msp/lib/Pareto/PriorityQueue.h
@@ -0,0 +1,36 @@
+/*
+ * Copyright Jakob Gruber, Martin Kalany 2013.
+ * Distributed under the Boost Software License, Version 1.0.
+ * (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+ */
+
+#ifndef __PRIORITY_QUEUE_H
+#define __PRIORITY_QUEUE_H
+
+#include "Queue.h"
+
+#include <queue>
+
+namespace pareto
+{
+
+/**
+ * An implementation of the pareto queue using a priority queue and lazy removal.
+ */
+class PriorityQueue : public Queue
+{
+public:
+	PriorityQueue();
+	virtual ~PriorityQueue();
+
+	std::vector<sp::PathPtr> first(size_t const n) override;
+	void insert(sp::PathPtr path) override;
+	bool empty() const override;
+
+private:
+	std::priority_queue<sp::PathPtr> m_queue;
+};
+
+}
+
+#endif /* __PRIORITY_QUEUE_H */
