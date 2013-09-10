a56d48dd460b72b7e48847c60d766c02000eed9c
Martin Wimmer <wimmer@par.tuwien.ac.at>
2013-09-10 11:34:20 +0200

    print performance counters

--- test/sssp/Strategy/StrategySsspPerformanceCounters.h
+++ test/sssp/Strategy/StrategySsspPerformanceCounters.h
@@ -37,10 +37,16 @@ public:
 	static void print_headers() {
 		BasicPerformanceCounter<Pheet, sssp_count_dead_tasks>::print_header("num_dead_tasks\t");
 		BasicPerformanceCounter<Pheet, sssp_count_actual_tasks>::print_header("num_actual_tasks\t");
+		LastTimePerformanceCounter<Pheet, sssp_measure_last_non_dead_time>::print_header("last_non_dead_time\t");
+		LastTimePerformanceCounter<Pheet, sssp_measure_last_task_time>::print_header("last_task_time\t");
+		LastTimePerformanceCounter<Pheet, sssp_measure_last_update_time>::print_header("last_update_time\t");
 	}
 	void print_values() {
 		num_dead_tasks.print("%d\t");
 		num_actual_tasks.print("%d\t");
+		last_non_dead_time.print("%f\t");
+		last_task_time.print("%f\t");
+		last_update_time.print("%f\t");
 	}
 
 	BasicPerformanceCounter<Pheet, sssp_count_dead_tasks> num_dead_tasks;
