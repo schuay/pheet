8ba9d70402556ed9d347d4e9a7dae570f895d9d4
Jakob Gruber <jakob.gruber@gmail.com>
2013-09-11 13:45:39 +0200

    MSP: A quick KDTree implementation

--- test/msp/lib/Pareto/KDTree.cpp
+++ test/msp/lib/Pareto/KDTree.cpp
@@ -6,40 +6,167 @@
 
 #include "KDTree.h"
 
+#include "assert.h"
+
+using namespace graph;
 using namespace sp;
 
 namespace pareto
 {
 
+/**
+ * The left branch is <=, the right is >.
+ */
 struct tree_t {
 	const PathPtr p;
 	tree_t* l;
 	tree_t* r;
+	bool active;
 };
+
+KDTree::
+KDTree() :
+	t(nullptr),
+	dominates()
+{
+}
+
+static void
+tree_delete(tree_t* t)
+{
+	if (!t) {
+		return;
+	}
+
+	tree_delete(t->l);
+	tree_delete(t->r);
+
+	delete t;
+}
+
+KDTree::
+~KDTree()
+{
+	tree_delete(t);
+}
+
+bool
+KDTree::
+dominated(tree_t const* t,
+          const size_t i,
+          const PathPtr path) const
+{
+	if (!t) {
+		return false;
+	}
+
+	if (t->active && dominates(t->p.get(), path.get())) {
+		return true;
+	}
+
+	const size_t j = (i + 1) % path->degree();
+
+	if (t->p->weight()[i] <= path->weight()[i]) {
+		if (dominated(t->r, j, path)) {
+			return true;
+		}
+	}
+
+	return dominated(t->l, j, path);
+}
 
 
 bool
 KDTree::
-dominated(const sp::PathPtr path) const
+dominated(const PathPtr path) const
 {
-	(void)path;
-	return false;
+	return dominated(t, 0, path);
 }
 
 void
 KDTree::
-prune(const sp::PathPtr path,
-      sp::Paths& pruned)
+prune(tree_t* t,
+      const size_t i,
+      const PathPtr path,
+      Paths& pruned)
 {
-	(void)path;
-	(void)pruned;
+	if (!t) {
+		return;
+	}
+
+	if (t->active && dominates(path.get(), t->p.get())) {
+		t->active = false;
+		pruned.push_back(t->p);
+	}
+
+	const size_t j = (i + 1) % path->degree();
+
+	if (t->p->weight()[i] >= path->weight()[i]) {
+		prune(t->l, j, path, pruned);
+	}
+
+	prune(t->r, j, path, pruned);
 }
 
 void
 KDTree::
-insert(const sp::PathPtr path)
+prune(const PathPtr path,
+      Paths& pruned)
 {
-	(void)path;
+	prune(t, 0, path, pruned);
+}
+
+void
+KDTree::
+insert(const PathPtr path)
+{
+	assert(path);
+
+	weight_vector_t const& ws = path->weight();
+
+	const size_t degree = path->degree();
+
+	tree_t** parentptr = &t;
+	tree_t* child = t;
+
+	for (size_t i = 0; child; i = (i + 1) % degree) {
+		if (ws[i] <= child->p->weight()[i]) {
+			parentptr = &child->l;
+			child = child->l;
+		} else {
+			parentptr = &child->r;
+			child = child->r;
+		}
+	}
+
+	*parentptr = new tree_t { path, nullptr, nullptr, true };
+}
+
+void
+KDTree::
+items(const tree_t* t, Paths& paths) const
+{
+	if (!t) {
+		return;
+	}
+
+	assert(t->p);
+
+	if (t->active) {
+		paths.push_back(t->p);
+	}
+
+	items(t->l, paths);
+	items(t->r, paths);
+}
+
+Paths
+KDTree::
+items() const
+{
+	Paths ps;
+	items(t, ps);
+	return ps;
 }
 
 }
