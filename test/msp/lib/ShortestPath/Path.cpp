c90c162753b2684876cf92ea016c5075ecd31a54
Jakob Gruber <jakob.gruber@gmail.com>
2013-09-25 14:37:33 +0200

    Revert "MSP: Precompute the path weight sum"
    This reverts commit 5625d848c411b8c57ae6e1466315e547b461570a.

--- test/msp/lib/ShortestPath/Path.cpp
+++ test/msp/lib/ShortestPath/Path.cpp
@@ -17,12 +17,7 @@ namespace sp
 
 Path::
 Path(graph::Node const* init)
-	: m_tail(init),
-	  m_head(init),
-	  m_pred(nullptr),
-	  m_weight_sum(0),
-	  m_degree(init->graph()->degree()),
-	  m_dominated(false)
+	: m_tail(init), m_head(init), m_pred(nullptr), m_degree(init->graph()->degree()), m_dominated(false)
 {
 	m_weight.resize(m_degree, 0);
 }
@@ -33,7 +28,6 @@ Path(const Path& that)
 	  m_head(that.m_head),
 	  m_pred(that.m_pred),
 	  m_weight(that.m_weight),
-	  m_weight_sum(that.m_weight_sum),
 	  m_degree(that.m_degree),
 	  m_dominated(false)
 {
@@ -53,7 +47,6 @@ step(Edge const* edge) const
 
 	for (size_t i = 0; i < ws.size(); i++) {
 		p->m_weight[i] += ws[i];
-		p->m_weight_sum += ws[i];
 	}
 
 	return p;
--- test/msp/lib/ShortestPath/Path.h
+++ test/msp/lib/ShortestPath/Path.h
@@ -40,10 +40,6 @@ public:
 		return m_weight;
 	}
 
-	inline graph::weight_t weight_sum() const {
-		return m_weight_sum;
-	}
-
 	inline size_t degree() const {
 		return m_degree;
 	}
@@ -59,7 +55,6 @@ private:
 	graph::Node const* m_head;
 	Path const* m_pred;
 	std::vector<graph::weight_t> m_weight;
-	graph::weight_t m_weight_sum;
 	const size_t m_degree;
 	std::atomic_bool m_dominated;
 };
