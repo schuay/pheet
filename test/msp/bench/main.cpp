7fb337b084bb62c9e5df8b342b4d47b20fc278dd
Jakob Gruber <jakob.gruber@gmail.com>
2013-09-25 12:15:14 +0200

    REBASEME: Minor reformatting in benchmarks

--- test/msp/bench/main.cpp
+++ test/msp/bench/main.cpp
@@ -17,14 +17,17 @@
 
 using namespace pheet;
 
-inline bool is_file(char const* name)
+static inline bool
+is_file(char const* name)
 {
 	struct stat s;
 
 	return (stat(name, &s) == 0 && (s.st_mode & S_IFREG));
 }
 
-void parse(char const* name, std::vector<std::string>& files)
+static void
+parse(char const* name,
+      std::vector<std::string>& files)
 {
 	struct stat s;
 
@@ -34,7 +37,7 @@ void parse(char const* name, std::vector<std::string>& files)
 			DIR* dp;
 			struct dirent* dirp;
 			if ((dp  = opendir(name)) == NULL) {
-				std::cout << "Error(" << errno << ") opening " << name << std::endl;
+				std::cerr << "Error(" << errno << ") opening " << name << std::endl;
 				return;
 			}
 
@@ -48,30 +51,25 @@ void parse(char const* name, std::vector<std::string>& files)
 			//it's a file
 			files.push_back(name);
 		} else {
-			std::cout << "\"" << name << "\" is not a file or a directory." << std::endl;
+			std::cerr << "\"" << name << "\" is not a file or a directory." << std::endl;
 		}
 	} else {
-		std::cout << "Skipping file \"" << name << "\" because it does not exist." << std::endl;
+		std::cerr << "Skipping file \"" << name << "\" because it does not exist." << std::endl;
 	}
-
-
 }
 
-void usage()
+static void
+usage()
 {
-	std::cout << "\n msp-bench [-n...] [--seq] [--strategy] [DIRECTORY...] [FILE...]\n\n"
+	std::cerr << "\n msp-bench [-n...] [--seq] [--strategy] [DIRECTORY...] [FILE...]\n\n"
 	          << "Benchmark multi-objective shortest path algorithms\n"
 	          << "PARAMETERS:\n"
 	          << "-n\t\t number of processors to be used for benchmarking\n"
 	          << "--seq\t\t benchmark the sequential algorithm."
 	          << " Benchmark will be run for n=1.\n"
 	          << "--strategy\t benchmark the parallel algorithm.\n\n";
-	exit(1);
+	exit(EXIT_FAILURE);
 }
-
-//Flags for strategies to be used
-static int sequential = 0;
-static int strategy = 0;
 
 int
 main(int argc, char** argv)
@@ -80,14 +78,15 @@ main(int argc, char** argv)
 	std::vector<int> nv;
 	std::vector<std::string> files;
 
+	//Flags for strategies to be used
+	int sequential = 0;
+	int strategy = 0;
+
 	while (1) {
 		static struct option long_options[] = {
-
-			//Options setting a flag
 			{"seq",     no_argument,       &sequential,   1},
-			{"strategymsp",   no_argument,       &strategy, 1},
-			//Options setting a parameter
-			{"number_of_cpus",  required_argument, 0, 'n'},
+			{"strategy",   no_argument,       &strategy, 1},
+			{"ncpus",  required_argument, 0, 'n'},
 			{0, 0, 0, 0}
 		};
 
@@ -107,17 +106,16 @@ main(int argc, char** argv)
 			try {
 				n = std::stoi(optarg);
 			} catch (const std::invalid_argument& e) {
-				std::cout << "Invalid argument for option -n: " << optarg << std::endl;
+				std::cerr << "Invalid argument for option -n: " << optarg << std::endl;
 				usage();
 			} catch (const std::out_of_range& e) {
-				std::cout << "Argument for option -n out of range: " << optarg << std::endl;
+				std::cerr << "Argument for option -n out of range: " << optarg << std::endl;
 				usage();
 			}
 			nv.push_back(n);
 			break;
 
 		case '?':
-			//getopt_long handles errors
 			usage();
 
 		default:
@@ -130,16 +128,17 @@ main(int argc, char** argv)
 			parse(argv[optind], files);
 		} while (++optind < argc);
 	} else {
-		std::cout << "No input files given." << std::endl;
-		exit(1);
+		std::cerr << "No input files given." << std::endl;
+		return EXIT_FAILURE;
 	}
 
 	if (strategy && nv.size() == 0) {
-		std::cout << "Number of processors needs to be specified when --strategy is given.\n";
-		exit(0);
+		std::cerr << "Number of processors needs to be specified when --strategy is given.\n";
+		return EXIT_FAILURE;
 	}
 
 	MspBenchmarks t;
 	t.run_benchmarks(sequential, strategy, nv, files);
-	exit(0);
+
+	return EXIT_SUCCESS;
 }
