baff2229769fe03da845ac7cdd1995fac210aa83
Martin Kalany <m.kalany@gmx.at>
2013-10-16 14:16:04 +0200

    extended ParetoLocalityTaskStorage classes

--- pheet/ds/StrategyTaskStorage/ParetoLocality/ParetoLocalityTaskStorage.h
+++ pheet/ds/StrategyTaskStorage/ParetoLocality/ParetoLocalityTaskStorage.h
@@ -7,6 +7,8 @@
 #ifndef PARETOLOCALITYTASKSTORAGE_H_
 #define PARETOLOCALITYTASKSTORAGE_H_
 
+#include "ParetoLocalityTaskStoragePlace.h"
+
 namespace pheet
 {
 
@@ -15,8 +17,69 @@ class ParetoLocalityTaskStorage : public Pheet::Scheduler::BaseTaskStorage
 {
 public:
 	typedef ParetoLocalityTaskStorage<Pheet, Strategy> Self;
+	typedef typename Strategy::BaseStrategy::TaskStorage ParentTaskStorage;
+	typedef typename ParentTaskStorage::BaseItem BaseItem;
+	typedef typename BaseItem::T T;
+	typedef ParetoLocalityTaskStoragePlace<Pheet, Self, typename ParentTaskStorage::Place, Strategy>
+	Place;
+
+	ParetoLocalityTaskStorage(ParentTaskStorage* parent);
+	~ParetoLocalityTaskStorage();
+
+	static Self* get(Place* place, ParentTaskStorage* parent, bool& created);
+
+	/*
+	 * Override from base class
+	 */
+	T pop(BaseItem* boundary, procs_t place_id);
+	/*
+	 * Override from base class
+	 */
+	T steal(BaseItem* boundary, procs_t place_id);
+
+	static void print_name() {
+		std::cout << "LSMLocalityTaskStorage";
+	}
 
 };
+
+template <class Pheet, class Strategy>
+ParetoLocalityTaskStorage<Pheet, Strategy>::
+ParetoLocalityTaskStorage(ParentTaskStorage* parent)
+{
+
+}
+
+template <class Pheet, class Strategy>
+ParetoLocalityTaskStorage<Pheet, Strategy>::
+~ParetoLocalityTaskStorage()
+{
+
+}
+
+template <class Pheet, class Strategy>
+typename ParetoLocalityTaskStorage<Pheet, Strategy>::Self*
+ParetoLocalityTaskStorage<Pheet, Strategy>::
+get(Place* place, ParentTaskStorage* parent, bool& created)
+{
+	return nullptr;
+}
+
+template <class Pheet, class Strategy>
+typename ParetoLocalityTaskStorage<Pheet, Strategy>::T
+ParetoLocalityTaskStorage<Pheet, Strategy>::
+pop(BaseItem* boundary, procs_t place_id)
+{
+	return nullptr;
+}
+
+template <class Pheet, class Strategy>
+typename ParetoLocalityTaskStorage<Pheet, Strategy>::T
+ParetoLocalityTaskStorage<Pheet, Strategy>::
+steal(BaseItem* boundary, procs_t place_id)
+{
+	return nullptr;
+}
 
 } /* namespace pheet */
 
--- pheet/ds/StrategyTaskStorage/ParetoLocality/ParetoLocalityTaskStoragePlace.h
+++ pheet/ds/StrategyTaskStorage/ParetoLocality/ParetoLocalityTaskStoragePlace.h
@@ -4,7 +4,8 @@
  * (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
  */
 
-#ifndef
+#ifndef PARETOLOCALITYTASKSTORAGEPLACE_H_
+#define PARETOLOCALITYTASKSTORAGEPLACE_H_
 
 #include "ParetoLocalityTaskStorageItem.h"
 
@@ -21,10 +22,82 @@ public:
 	typedef ParetoLocalityTaskStoragePlace<Pheet, TaskStorage, ParentTaskStoragePlace, Strategy> Self;
 	typedef typename ParentTaskStoragePlace::BaseItem BaseItem;
 	typedef ParetoLocalityTaskStorageItem<Pheet, Self, BaseItem, Strategy> Item;
+	typedef typename BaseItem::T T;
+
+	ParetoLocalityTaskStoragePlace(ParentTaskStoragePlace* parent);
+	~ParetoLocalityTaskStoragePlace();
 
 	void push(Strategy&& strategy, T data);
 	T pop(BaseItem* boundary);
 	T steal(BaseItem* boundary);
+	void clean_up();
 
+};
+
+template < class Pheet,
+         class TaskStorage,
+         class ParentTaskStoragePlace,
+         class Strategy >
+ParetoLocalityTaskStoragePlace<Pheet, TaskStorage, ParentTaskStoragePlace, Strategy>::
+ParetoLocalityTaskStoragePlace(ParentTaskStoragePlace* parent)
+{
 
 }
+
+template < class Pheet,
+         class TaskStorage,
+         class ParentTaskStoragePlace,
+         class Strategy >
+ParetoLocalityTaskStoragePlace<Pheet, TaskStorage, ParentTaskStoragePlace, Strategy>::
+~ParetoLocalityTaskStoragePlace()
+{
+
+}
+
+template < class Pheet,
+         class TaskStorage,
+         class ParentTaskStoragePlace,
+         class Strategy >
+void
+ParetoLocalityTaskStoragePlace<Pheet, TaskStorage, ParentTaskStoragePlace, Strategy>::
+push(Strategy&& strategy, T data)
+{
+
+}
+
+template < class Pheet,
+         class TaskStorage,
+         class ParentTaskStoragePlace,
+         class Strategy >
+typename ParetoLocalityTaskStoragePlace<Pheet, TaskStorage, ParentTaskStoragePlace, Strategy>::T
+ParetoLocalityTaskStoragePlace<Pheet, TaskStorage, ParentTaskStoragePlace, Strategy>::
+pop(BaseItem* boundary)
+{
+
+}
+
+template < class Pheet,
+         class TaskStorage,
+         class ParentTaskStoragePlace,
+         class Strategy >
+typename ParetoLocalityTaskStoragePlace<Pheet, TaskStorage, ParentTaskStoragePlace, Strategy>::T
+ParetoLocalityTaskStoragePlace<Pheet, TaskStorage, ParentTaskStoragePlace, Strategy>::
+steal(BaseItem* boundary)
+{
+
+}
+
+template < class Pheet,
+         class TaskStorage,
+         class ParentTaskStoragePlace,
+         class Strategy >
+void
+ParetoLocalityTaskStoragePlace<Pheet, TaskStorage, ParentTaskStoragePlace, Strategy>::
+clean_up()
+{
+
+}
+
+} /* namepsace pheet */
+
+#endif /* PARETOLOCALITYTASKSTORAGEPLACE_H_ */
