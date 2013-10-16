d8bde7761f866ffa50ca002d0127ebabf71bb7e5
Martin Kalany <m.kalany@gmx.at>
2013-10-16 17:43:43 +0200

    implementations for place initialisation, pop and steal
    copied from KLSM for now, since semantics seem to be the same

--- pheet/ds/StrategyTaskStorage/ParetoLocality/ParetoLocalityTaskStorage.h
+++ pheet/ds/StrategyTaskStorage/ParetoLocality/ParetoLocalityTaskStorage.h
@@ -26,6 +26,10 @@ public:
 	ParetoLocalityTaskStorage(ParentTaskStorage* parent);
 	~ParetoLocalityTaskStorage();
 
+	/*
+	 * Called by each place exactly once. Only one place will get created == true
+	 * This place is responsible for cleanup of base task storage
+	 */
 	static Self* get(Place* place, ParentTaskStorage* parent, bool& created);
 
 	/*
@@ -40,6 +44,7 @@ public:
 	static void print_name();
 
 private:
+	static std::atomic<Self*> singleton;
 	ParentTaskStorage* parent;
 	std::atomic<Place*>* places;
 };
@@ -68,7 +73,27 @@ typename ParetoLocalityTaskStorage<Pheet, Strategy>::Self*
 ParetoLocalityTaskStorage<Pheet, Strategy>::
 get(Place* place, ParentTaskStorage* parent, bool& created)
 {
-	return nullptr;
+	Self* ret = singleton.load(std::memory_order_acquire);
+	if (ret == nullptr) {
+		// We try to create a task storage
+		ret = new Self(parent);
+
+		Self* expected = nullptr;
+		if (singleton.compare_exchange_strong(expected, ret, std::memory_order_acq_rel,
+		                                      std::memory_order_relaxed)) {
+			created = true;
+		} else {
+			// Conflict on initialization, need to delete doubly initialized object
+			created = false;
+			delete ret;
+			pheet_assert(expected != nullptr);
+			ret = expected;
+		}
+	} else {
+		created = false;
+	}
+	ret->places[Pheet::get_place_id()].store(place, std::memory_order_release);
+	return ret;
 }
 
 template <class Pheet, class Strategy>
@@ -76,7 +101,16 @@ typename ParetoLocalityTaskStorage<Pheet, Strategy>::T
 ParetoLocalityTaskStorage<Pheet, Strategy>::
 pop(BaseItem* boundary, procs_t place_id)
 {
-	return nullptr;
+	// Should only be called by the owner
+	pheet_assert(place_id == Pheet::get_place_id());
+
+	// Load place.
+	Place* place = places[place_id].load(std::memory_order_relaxed);
+	// Should already be filled, otherwise there would be no pop
+	pheet_assert(place != nullptr);
+	pheet_assert(place == Pheet::get_place()->template get_task_storage<Strategy>());
+
+	return place->pop(boundary);
 }
 
 template <class Pheet, class Strategy>
@@ -84,7 +118,17 @@ typename ParetoLocalityTaskStorage<Pheet, Strategy>::T
 ParetoLocalityTaskStorage<Pheet, Strategy>::
 steal(BaseItem* boundary, procs_t place_id)
 {
-	return nullptr;
+	// Should be called by everyone except the owner
+	pheet_assert(place_id != Pheet::get_place_id());
+
+	// stealing is called in the context of the stealer, therefore need to retrieve place
+	// from Scheduler (might not yet have been initialized)
+	Place* place = Pheet::get_place()->template get_task_storage<Strategy>();
+	// Place should be created if it does not exist
+	pheet_assert(place != nullptr);
+	pheet_assert(place == places[Pheet::get_place_id()]);
+
+	return place->steal(boundary);
 }
 
 template <class Pheet, class Strategy>
