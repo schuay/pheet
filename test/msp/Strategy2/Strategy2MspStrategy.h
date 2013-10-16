9076e620e527f6b5f49f7c6b9f8df6739e09bb10
Martin Kalany <m.kalany@gmx.at>
2013-10-16 14:33:37 +0200

    add basic class definitions for Strategy2Msp variant

---
+++ test/msp/Strategy2/Strategy2MspStrategy.h
@@ -0,0 +1,98 @@
+/*
+ * Copyright Jakob Gruber 2013.
+ * Distributed under the Boost Software License, Version 1.0.
+ * (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+ */
+
+#ifndef STRATEGY2MSPSTRATEGY_H_
+#define STRATEGY2MSPSTRATEGY_H_
+
+#include "lib/ShortestPath/Path.h"
+
+namespace pheet
+{
+
+template <class Pheet, template <class, class> class TaskStorageT>
+class Strategy2MspStrategy
+	: public Pheet::Environment::BaseStrategy
+{
+public:
+	typedef Strategy2MspStrategy<Pheet, TaskStorageT> Self;
+	typedef typename Pheet::Environment::BaseStrategy BaseStrategy;
+	typedef TaskStorageT<Pheet, Self> TaskStorage;
+	typedef typename TaskStorage::Place TaskStoragePlace;
+	typedef typename Pheet::Place Place;
+
+	Strategy2MspStrategy(sp::PathPtr path);
+	Strategy2MspStrategy(Self& other);
+	Strategy2MspStrategy(Self&& other);
+
+	inline bool prioritize(Self& other) const;
+	inline bool forbid_call_conversion() const;
+	inline bool dead_task();
+	inline bool can_call(TaskStoragePlace*);
+
+private:
+	sp::PathPtr path;
+};
+
+template <class Pheet, template <class, class> class TaskStorageT>
+Strategy2MspStrategy<Pheet, TaskStorageT>::
+Strategy2MspStrategy(sp::PathPtr path)
+	: path(path)
+{
+}
+
+template <class Pheet, template <class, class> class TaskStorageT>
+Strategy2MspStrategy<Pheet, TaskStorageT>::
+Strategy2MspStrategy(Self& other)
+	: BaseStrategy(other),
+	  path(other.path)
+{
+}
+
+template <class Pheet, template <class, class> class TaskStorageT>
+Strategy2MspStrategy<Pheet, TaskStorageT>::
+Strategy2MspStrategy(Self&& other)
+	: BaseStrategy(other),
+	  path(other.path)
+{
+}
+
+template <class Pheet, template <class, class> class TaskStorageT>
+inline bool
+Strategy2MspStrategy<Pheet, TaskStorageT>::
+prioritize(Self& other) const
+{
+	return (path->weight_sum() < other.path->weight_sum());
+}
+
+template <class Pheet, template <class, class> class TaskStorageT>
+inline bool
+Strategy2MspStrategy<Pheet, TaskStorageT>::
+forbid_call_conversion() const
+{
+	return true;
+}
+
+template <class Pheet, template <class, class> class TaskStorageT>
+inline bool
+Strategy2MspStrategy<Pheet, TaskStorageT>::
+dead_task()
+{
+	return path->dominated();
+}
+
+template <class Pheet, template <class, class> class TaskStorageT>
+inline bool
+Strategy2MspStrategy<Pheet, TaskStorageT>::
+can_call(TaskStoragePlace*)
+{
+	return false;
+}
+
+
+} /* namespace pheet */
+
+#endif /* STRATEGY2MSPSTRATEGY_H_ */
+
---
+++ test/msp/Strategy2/Strategy2MspTask.h
@@ -0,0 +1,99 @@
+/*
+ * Copyright Jakob Gruber 2013.
+ * Distributed under the Boost Software License, Version 1.0.
+ * (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+ */
+
+#ifndef STRATEGY2MSPTASK_H_
+#define STRATEGY2MSPTASK_H_
+
+#include "lib/Pareto/Sets.h"
+#include "MspPerformanceCounters.h"
+#include "pheet/ds/StrategyTaskStorage/ParetoLocality/ParetoLocalityTaskStorage.h"
+#include "Strategy2MspStrategy.h"
+
+namespace pheet
+{
+
+template <class Pheet>
+class Strategy2MspTask : public Pheet::Task
+{
+public:
+	typedef Strategy2MspTask<Pheet> Self;
+	typedef Strategy2MspStrategy<Pheet, ParetoLocalityTaskStorage> Strategy;
+	typedef MspPerformanceCounters<Pheet> PerformanceCounters;
+
+
+	Strategy2MspTask(const graph::Graph* graph,
+	                 const sp::PathPtr path,
+	                 pareto::Sets* sets,
+	                 PerformanceCounters& pc);
+
+	virtual void operator()();
+
+	static char const name[];
+
+
+private:
+	const graph::Graph* graph;
+	const sp::PathPtr path;
+	pareto::Sets* sets;
+	PerformanceCounters& pc;
+};
+
+template <class Pheet>
+char const Strategy2MspTask<Pheet>::
+name[] = "Strategy2 Msp";
+
+template <class Pheet>
+Strategy2MspTask<Pheet>::
+Strategy2MspTask(const graph::Graph* graph,
+                 const sp::PathPtr path,
+                 pareto::Sets* sets,
+                 PerformanceCounters& pc)
+	: graph(graph), path(path), sets(sets), pc(pc)
+{
+}
+
+template <class Pheet>
+void
+Strategy2MspTask<Pheet>::
+operator()()
+{
+	if (path->dominated()) {
+		return;
+	}
+
+	pc.num_actual_tasks.incr();
+
+	StrategyMspData& d = Pheet::template place_singleton<StrategyMspData>();
+	d.clear();
+
+	/* Generate candidates. */
+	const graph::Node* head = path->head();
+
+	d.candidates.reserve(head->out_edges().size());
+	for (auto & e : head->out_edges()) {
+		sp::PathPtr to(path->step(e));
+		d.candidates.push_back(to);
+	}
+
+	/* Insert into the Pareto set. Mark dominated paths and spawn tasks for
+	 * newly added paths. */
+
+	sets->insert(d.candidates, d.added, d.removed);
+
+	for (sp::PathPtr & p : d.removed) {
+		p->set_dominated();
+		pc.num_dead_tasks.incr();
+	}
+
+	for (sp::PathPtr & p : d.added) {
+		Pheet::template spawn_s<Self>(Strategy(p), graph, p, sets, pc);
+	}
+}
+
+} /* namespace pheet */
+
+#endif /* STRATEGY2MSPTASK_H_ */
+
