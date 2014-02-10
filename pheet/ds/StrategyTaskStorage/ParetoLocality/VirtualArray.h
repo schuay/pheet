34858dbb04fcaeae06a51e75456bb606a62736bc
Jakob Gruber <jakob.gruber@gmail.com>
2014-02-10 13:47:20 +0100

    Initial VirtualArray iterator code

--- pheet/ds/StrategyTaskStorage/ParetoLocality/VirtualArray.h
+++ pheet/ds/StrategyTaskStorage/ParetoLocality/VirtualArray.h
@@ -8,6 +8,8 @@
 #ifndef VIRTUAL_ARRAY_H_
 #define VIRTUAL_ARRAY_H_
 
+#include <iterator>
+
 #include "VirtualArrayBlock.h"
 
 #define DATA_BLOCK_SIZE (100)
@@ -22,6 +24,98 @@ template <class T>
 class VirtualArray
 {
 public:
+	typedef VirtualArrayBlock<T, DATA_BLOCK_SIZE> Block;
+
+	class VirtualArrayIterator : public std::iterator<std::random_access_iterator_tag, T>
+	{
+		friend class VirtualArray<T>;
+	public:
+		VirtualArrayIterator() : m_block(nullptr), m_block_nr(0), m_idx(0)
+		{
+		}
+
+		VirtualArrayIterator(const VirtualArrayIterator& that)
+			: m_block(that.m_block), m_block_nr(that.m_block_nr), m_idx(that.m_idx)
+		{
+		}
+
+		VirtualArrayIterator& operator=(const VirtualArrayIterator& that)
+		{
+			m_block = that.m_block;
+			m_block_nr = that.m_block_nr;
+			m_idx = that.m_idx;
+			return *this;
+		}
+
+		VirtualArrayIterator operator++(int)
+		{
+			if (m_block == nullptr) {
+				return *this;
+			}
+
+			VirtualArrayIterator orig(*this);
+
+			m_idx++;
+			if (m_idx >= m_block->capacity()) {
+				m_block = m_block->next;
+				m_block_nr++;
+				m_idx = 0;
+			}
+
+			return orig;
+		}
+
+		VirtualArrayIterator& operator--()
+		{
+			if (m_block == nullptr) {
+				return *this;
+			}
+
+			if (m_idx == 0) {
+				m_block = m_block->prev;
+				m_block_nr--;
+				m_idx = (m_block == nullptr) ? 0 : m_block->capacity() - 1;
+			} else {
+				m_idx--;
+			}
+
+			return *this;
+		}
+
+		T& operator*() const
+		{
+			return m_block->operator [](m_idx);
+		}
+
+		bool operator==(const VirtualArrayIterator& that)
+		{
+			return (m_block == that.m_block && m_idx == that.m_idx);
+		}
+
+		bool operator!=(const VirtualArrayIterator& that)
+		{
+			return !operator ==(that);
+		}
+
+		bool operator<(const VirtualArrayIterator& that)
+		{
+			if (m_block_nr < that.m_block_nr) {
+				return true;
+			}
+			if (m_block_nr > that.m_block_nr) {
+				return false;
+			}
+			return (m_idx < that.m_idx);
+		}
+
+//		friend void swap(iterator& lhs, iterator& rhs);
+
+	private:
+		VirtualArray::Block* m_block;
+		ssize_t m_block_nr;	/**< The nr of the current block within the ordered sequence. */
+		size_t m_idx;
+	};
+
 	VirtualArray()
 		: m_block_cnt(1)
 	{
@@ -37,6 +131,38 @@ public:
 		delete m_first;
 	}
 
+	VirtualArrayIterator begin() const
+	{
+		VirtualArrayIterator it;
+		it.m_block = m_first;
+	}
+
+	VirtualArrayIterator iterator_to(const size_t idx) const
+	{
+		if (idx >= size()) {
+			return end();
+		}
+
+		Block* block = find_block(idx);
+
+		VirtualArrayIterator it;
+		it.m_block = block;
+		it.m_idx = idx % block_size();
+
+		for (const Block* i = m_first; i != block; i = i->next) {
+			it.m_block_nr++;
+		}
+
+		return it;
+	}
+
+	VirtualArrayIterator end() const
+	{
+		VirtualArrayIterator it;
+		it.m_block_nr = m_block_cnt;
+		return it;
+	}
+
 	void push(T item)
 	{
 		if (m_last->size() == m_last->capacity()) {
@@ -50,7 +176,7 @@ public:
 	{
 		pheet_assert(idx < size());
 
-		Block* block =  find_block(idx);
+		Block* block = find_block(idx);
 		return (*block)[idx % block_size()];
 	}
 
@@ -77,9 +203,6 @@ public:
 	{
 		return DATA_BLOCK_SIZE;
 	}
-
-private:
-	typedef VirtualArrayBlock<T, DATA_BLOCK_SIZE> Block;
 
 private:
 	Block* find_block(size_t idx) const
